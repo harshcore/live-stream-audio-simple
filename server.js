ac67fb836b93c843bdd76670a050babeb93b60d1
harshcore <153198322+harshcore@users.noreply.github.com>
2024-05-24 17:09:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const { parseFile } = require("music-metadata");
 const express = require("express");
 const Throttle = require("throttle");
 const Fs = require("fs");
+const axios = require("axios");
+const { http, https } = require("follow-redirects");
+const urls = require("./urls.json");
 const { PassThrough } = require("stream");
 
 const app = express();
-let filePath = "./audio.mp3"; // put your favorite audio file
+let filePath = "./audio.mp3";
 let bitRate = 0;
 const streams = new Map();
-// app.use(express.static("public"));
 
 app.get("/stream", (req, res) => {
-  const { id, stream } = generateStream(); // We create a new stream for each new client
+  const { id, stream } = generateStream();
   res.setHeader("Content-Type", "audio/mpeg");
-  stream.pipe(res); // the client stream is piped to the response
+  stream.pipe(res);
   res.on("close", () => {
     streams.delete(id);
   });
@@ -23,27 +25,42 @@ app.get("/life", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-const init = async () => {
-  const fileInfo = await parseFile(filePath);
-  bitRate = fileInfo.format.bitrate / 8;
-};
+const init = async () => {};
 
 const playFile = (filePath) => {
-  const songReadable = Fs.createReadStream(filePath);
-  const throttleTransformable = new Throttle(bitRate);
-  songReadable.pipe(throttleTransformable);
-  throttleTransformable.on("data", (chunk) => {
-    broadcastToEveryStreams(chunk);
+  var randomIndex = Math.floor(Math.random() * urls.length);
+  const url = urls[randomIndex];
+  const request = https.get(url, (response) => {
+    if (response.statusCode === 200) {
+      const songReadable = response;
+
+      const bitRate = 128 * 1024;
+      const throttleTransformable = new Throttle(bitRate);
+      songReadable.pipe(throttleTransformable);
+
+      throttleTransformable.on("data", (chunk) => {
+        broadcastToEveryStreams(chunk);
+      });
+      throttleTransformable.on("error", (e) => console.log(e));
+      throttleTransformable.on("end", () => {
+        playFile(filePath);
+      });
+    } else {
+      console.error(
+        "Failed to download the file. Status code:",
+        response.statusCode
+      );
+    }
   });
-  throttleTransformable.on("error", (e) => console.log(e));
-  throttleTransformable.on("end", () => {
-    playFile(filePath); // Replay the file once it ends
+
+  request.on("error", (error) => {
+    console.error("Error downloading the file:", error);
   });
 };
 
 const broadcastToEveryStreams = (chunk) => {
   for (let [id, stream] of streams) {
-    stream.write(chunk); // We write to the client stream the new chunk of data
+    stream.write(chunk);
   }
 };
 
